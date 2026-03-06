c4fd679ec130ef66c6da706b6ce4c675f058d1ab
SandyArdiyan <sanardiyan881@gmail.com>
2026-03-06 20:30:08 +0700

    Membuat background

--- tugas3/lib/stateful.dart
+++ tugas3/lib/stateful.dart
@@ -3,3 +3,12 @@ import 'package:flutter/material.dart';
 class CounterManager extends StatefulWidget {
   const CounterManager({super.key});
 
+@override
+  State<CounterManager> createState() => _CounterManagerState();
+}
+
+class _CounterManagerState extends State<CounterManager> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFAFAFA), // Warna background abu-abu sangat terang
