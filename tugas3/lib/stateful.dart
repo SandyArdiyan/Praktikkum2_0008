136658ca3bafa90c62b622bf690bb48bb498b137
SandyArdiyan <sanardiyan881@gmail.com>
2026-03-06 20:31:15 +0700

    Membuat Appbar Back

--- tugas3/lib/stateful.dart
+++ tugas3/lib/stateful.dart
@@ -12,3 +12,23 @@ class _CounterManagerState extends State<CounterManager> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFFAFAFA), // Warna background abu-abu sangat terang
+
+ // --- APPBAR (Tombol Back & Setting melayang) ---
+      appBar: AppBar(
+        backgroundColor: Colors.transparent, // Bikin transparan agar menyatu dengan background
+        elevation: 0, // Hilangkan bayangan
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black87),
+          onPressed: () {
+            // Aksi tombol back
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings, color: Colors.black87),
+            onPressed: () {
+              // Aksi tombol setting
+            },
+          ),
+        ],
+      ),
